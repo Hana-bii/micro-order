afeacf27d772a1c68c50b52478bc0e7cd7d35147
Hanabi <1357984328@qq.com>
2025-02-20 15:56:38 +0800

    order stock inmen repo

---
+++ internal/stock/adapters/stock_inmen_repository.go
@@ -0,0 +1,49 @@
+package adapters
+
+import (
+	"context"
+	"github.com/Hana-bii/gorder-v2/common/genproto/orderpb"
+	domain "github.com/Hana-bii/gorder-v2/stock/domain/stock"
+	"sync"
+)
+
+type MemoryStockRepository struct {
+	lock  *sync.RWMutex
+	store map[string]*orderpb.Item
+}
+
+var stub = map[string]*orderpb.Item{
+	"item_id": {
+		ID:       "foo_item",
+		Name:     "stub item",
+		Quantity: 10000,
+		PriceID:  "stub_item_price_id",
+	},
+}
+
+func (m MemoryStockRepository) GetItems(ctx context.Context, ids []string) ([]*orderpb.Item, error) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	var (
+		res     []*orderpb.Item
+		missing []string
+	)
+	for _, id := range ids {
+		if item, ok := m.store[id]; ok {
+			res = append(res, item)
+		} else {
+			missing = append(missing, id)
+		}
+	}
+	if len(res) == len(ids) {
+		return res, nil
+	}
+	return res, domain.NotFoundError{Missing: missing}
+}
+
+func NewMemoryStockRepository() *MemoryStockRepository {
+	return &MemoryStockRepository{
+		store: stub,
+		lock:  &sync.RWMutex{},
+	}
+}
